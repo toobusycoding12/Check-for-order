13a0eea756a5ad0d2cee763cf213898825daca7a
toobusycoding12 <34730814+toobusycoding12@users.noreply.github.com>
2022-06-05 10:04:38 -0500

    Add files via upload

---
+++ automate_csv.py
@@ -0,0 +1,58 @@
+#-------------------------------------------------------------------------------
+# Imports
+import csv
+from selenium import webdriver
+import time
+
+#-------------------------------------------------------------------------------
+# Setup
+
+tokenid = 0
+
+with open('2000-2499.csv', 'r') as csv_file:
+
+    csv_reader = csv.reader(csv_file)
+
+#-------------------------------------------------------------------------------
+# Web Automation
+
+    driver = webdriver.Chrome()
+    driver.get('https://goblintown.wtf/mcgoblin/')
+
+    time.sleep(5)
+
+    enter = driver.find_element_by_xpath('//*[@id="Enter"]')
+    enter.click()
+
+    time.sleep(3)
+
+    connect = driver.find_element_by_xpath('//*[@id="Connect"]')
+    connect.click()
+
+    time.sleep(10)
+
+    for line in csv_reader:
+
+        tokenid_field = driver.find_element_by_xpath('//*[@id="check"]')
+        tokenid_field.click()
+        tokenid_field.clear()
+        time.sleep(1)
+        tokenid_field.send_keys(line[0])
+
+        time.sleep(1)
+
+        submit = driver.find_element_by_xpath('//*[@id="Avail-Btn"]')
+        submit.click()
+
+        time.sleep(1)
+
+        print(driver.find_element_by_xpath('//*[@id="Avail-Btn"]').text)
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------
